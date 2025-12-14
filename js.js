ca24da7bafc2b40a1caf78960328ead50c2a886a
Lataj2011 <latajdmitro2011@gmail.com>
2025-12-14 17:53:02 +0200

    full code of: css & js

--- js.js
+++ js.js
@@ -1,9 +1,9 @@
 let productsGrid = document.getElementById("products-grid")
 let productsArray = [];
-let xhr = new.XMLHttpRequest()
+let xhr = new XMLHttpRequest();
 let url = "https://my-json-server.typicode.com/lyrenta/w"
 
-xhr.open('GET'.url + "/products");
+xhr.open('GET', url + "/products");
 xhr.responseType = "json"
 xhr.onload = function () {
     productsArray = xhr.response
@@ -45,5 +45,40 @@ function addProductToCart(id) {
     let product = productsArray.find(function(p) {
         return p.id == id;
     })
-    cart
+    cart.push(product);
+    drawCartProducts();
+    localStorage.setItem("cart", JSON.stringify(cart))
+
+    document.getElementById("cart-button").classList.add("active")
+    setTimeout(function() {
+        document.getElementById("cart-button").classList.remove("active")
+    }, 500)
+}
+
+function drawCartProducts() {
+    if (cart.length === 0) return cartProd.innerHTML - "Cart is empty"
+    cartProd.innerHTML = null;
+    let sum = 0;
+    cart.forEach(function() {
+        cartProd.innerHTML = `
+            <p><img src="${p.photo_url}"> ${p.name} !${p.price}$</p>
+            <hr>
+        `;
+        sum += price;
+    })
+    cartProd.innerHTML = `
+        <p>Total price: ${sum}</p>
+        <button onclick="buyAll()">Buy All</button>
+    `;
+    
+}
+
+function buyAll() {
+    cart = [];
+    cartProd.innerHTML += `Money was withdrawn from your credit card`;
+    localStorage.setItem("cart", "[]")
+}
+
+function openCart() {
+    cartProd.classList.toggle("hide")
 }
